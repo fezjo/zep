06bf288336614c08941ece902a2267175d284a18
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-04-16 14:39:35 +0200

    Fix console after fs::path migration

--- include/zep/imgui/console_imgui.h
+++ include/zep/imgui/console_imgui.h
@@ -28,7 +28,7 @@ struct ZepConsole : Zep::IZepComponent
         return (Zep::ZepEditor&)zepEditor;
     }
 
-    ZepConsole(fs::path& p)
+    ZepConsole(Zep::fs::path& p)
         : zepEditor(p, Zep::NVec2f(1.0f))
     {
         zepEditor.RegisterCallback(this);
